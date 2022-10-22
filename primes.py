6968a21eb5a36a95f6ba7daf887ff4fae2751c16
jummanabedin <91836600+jummanabedin@users.noreply.github.com>
2022-10-22 15:06:13 +0100

    commit

--- primes.py
+++ primes.py
@@ -1,7 +1,10 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"number_of_primes={number_of_primes} should have been positive number")
     list = []
     n = 2
     while len(list) != number_of_primes:
@@ -10,5 +13,5 @@ def primes(number_of_primes):
                 break
         else:
             list.append(n)
-        n += 1
+            n += 1
     return list
