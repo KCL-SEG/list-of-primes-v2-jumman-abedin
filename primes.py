e8d95cb63343ae58ad38f389e68fcb5b0c5df4f6
jummanabedin <91836600+jummanabedin@users.noreply.github.com>
2022-10-22 14:42:20 +0100

    v1

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) != number_of_primes:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                break
+        else:
+            list.append(n)
+        n += 1
     return list
